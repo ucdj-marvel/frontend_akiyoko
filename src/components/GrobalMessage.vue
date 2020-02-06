f5f367d441e5ff67e0c6f8d333f822ee908b402b
Junta Uchida <uchida@qrestia.com>
2020-02-06 15:05:34 +0900

    add key

--- src/components/GrobalMessage.vue
+++ src/components/GrobalMessage.vue
@@ -1,14 +1,33 @@
 <template>
   <!-- メッセージエリア -->
   <div id="messages">
-    <b-alert variant="danger" show v-show="message.error" class="mb-0">
+    <b-alert
+      variant="danger"
+      show
+      v-show="message.error"
+      class="mb-0"
+    >
       {{ message.error }}
     </b-alert>
     <b-alert
-      variant="warning" show v-show="message.warnings.length > 0" class="mb-0">
-      <p v-for="warning in message.warnings" class="mb-0">{{ warning }}</p>
+      variant="warning"
+      show
+      v-show="message.warnings.length > 0"
+      class="mb-0"
+    >
+      <p
+        v-for="(warning, index) in message.warnings"
+        :key="index"
+        class="mb-0"
+      >
+        {{ warning }}
+      </p>
     </b-alert>
-    <b-alert variant="info" show v-show="message.info" class="mb-0">
+    <b-alert
+      variant="info"
+      show v-show="message.info"
+      class="mb-0"
+    >
       {{ message.info }}
     </b-alert>
   </div>
